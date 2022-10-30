b549153d8bca4dedbf291ee87736b84706e603eb
Muhammad_Arsalan Iqbal <116029293+arsalan0002@users.noreply.github.com>
2022-10-30 23:09:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,193 @@
+alert("Hello World..!");
+
+alert("Wellcome to javascript");
+
+alert("Thanks for your input!");
+
+window.alert("Another way");
+
+var Name = "Ali";
+alert(Name)
+
+var Name2 = "SAIMS"
+alert(Name2)
+
+var nationality;
+ nationality = "Pakistan";
+ alert(nationality);
+
+var weight1 = 200;
+var newweight = weight1 + 100;
+alert(newweight);
+
+var weight1 = 150;
+var newweight = 50 + weight1;
+alert(newweight);
+
+var weight1 = 400;
+var weight2 = 200;
+var newweight = weight1 + weight2;
+alert(newweight);
+
+var a = 10;
+var b = 20;
+var c = a+b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a-b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a*b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a/b;
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = a + b-a * b/a;
+console.log(c);
+
+var a = 20;
+a++;
+alert(a)
+
+var x = 4;
+console.log(x);
+var popularNumber = 2 + 2;
+console.log(x);
+console.log(2+2);
+ 
+var y = 12 - 24;
+console.log(y);
+
+var z = 3 * 12;
+console.log(z);
+
+ var num = 10;
+ var popularNumber = num + 200;
+ console.log(popularNumber);
+ 
+ var num = 10; 
+ var anotherNum = 1; 
+ var popularNumber = num + anotherNum;
+ console.log(popularNumber);
+
+var num = 60;
+var newNum = num++;
+console.log(newNum);
+console.log(num);
+
+var num = 60;
+var newNum = ++num;
+console.log(newNum);
+
+var num = 60;
+var newNum = num--;
+console.log(newNum);
+console.log(num);
+
+var 1stnum = 10; // illegal
+console.log(1stnum);
+
+var num1st = 10; // illegal
+console.log(num1st);
+
+var num = 60;
+var newNum = --num;
+console.log(newNum);
+
+var m = 20;
+var n = 30;
+var o = m++ + n;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = ++m + n++;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = ++m + ++n;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = m-- - n;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = --m - n--;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = --m - --n;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = m++ * ++n;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = ++m * n++;
+console.log(o);
+
+var m = 20;
+var n = 30;
+var o = ++m * ++n;
+console.log(o);
+
+var a = 20;
+var b = 30;
+var c = 40;
+var d = a++ - b * ++c - c + ++b + a + c;
+console.log(d);
+
+var totalCost = 1 + 3 * 4;
+console.log(totalCost);
+
+var totalCost = 1 + (3 * 4);
+console.log(totalCost);
+
+var totalCost = (1 + 3) * 4;
+console.log(totalCost);
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+console.log(resultOfComputation);
+
+var resultOfComputation = ((2 * 4) * 4) + 2;
+console.log(resultOfComputation);
+
+var resultOfComputation = (2 * 4) * (4 + 2);
+console.log(resultOfComputation);
+
+var message = "Thanks for your input!";
+console.log(message);
+
+alert("Thanks, " + "!");
+
+var message = "Thanks,";
+var banger = "!";
+alert(message + banger);
+
+alert("Thanks, " + "Susan" + "!");
+
+var message = "Thanks, "; var userName = "Susan";
+ var banger = "!";
+var customMess = message + userName + banger;
+window.alert(customMess);
+
+alert("2" + "2");
+
+alert("2 plus 2 equals " + 2 + 2);
